c8383a0e1644c1ec0f766d73a9f037735def3b5e
wonjun-dev <wonjun.dev@gmail.com>
2021-01-08 11:50:04 +0900

    models > nets.py: define nn

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Generative Adversarial Nets
+
+### Pytorch implementation of [Generatvie Adversarial Nets(GAN)](https://arxiv.org/abs/1406.2661).
---
+++ models/nets.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        # Define layers
+        pass
+
+    def forward(self, x):
+        pass
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        # Define layers
+        pass
+
+    def forward(self, x):
+        # Define forward pass
+        pass
