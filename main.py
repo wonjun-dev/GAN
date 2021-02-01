21dbdd4120dbbfca3477bd0d07d64759b14ee9e2
wonjun-dev <wonjun.dev@gmail.com>
2021-02-01 14:34:12 +0900

    tqdm progress bar

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 from models.nets import Generator, Discriminator
 
+# parser
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=100, help="number of sepochs")
 parser.add_argument("--batch_size", type=int, default=128)
@@ -28,6 +29,8 @@ parser.add_argument("--latent_dim", type=int, default=100, help="dimension of no
 parser.add_argument("--expr_name", type=str, default="baseline", help="name of the experiment")
 opt = parser.parse_args()
 print("Running Options: \n", opt)
+
+# Tensorboard
 writer = SummaryWriter(f"runs/{opt.dataset}/{opt.expr_name}")
 
 
@@ -35,68 +38,80 @@ def train():
     running_loss_G = 0.0
     running_loss_D = 0.0
 
-    for epoch in tqdm(range(opt.n_epochs)):
-        for i, (real_imgs, _) in enumerate(train_loader):
-            if device == "cuda":
-                real_imgs = real_imgs.cuda()
+    with tqdm(total=opt.n_epochs) as pbar:
+        for epoch in range(opt.n_epochs):
+            for i, (real_imgs, _) in enumerate(train_loader):
+                if device == "cuda":
+                    real_imgs = real_imgs.cuda()
 
-            # Target
-            real = torch.ones(real_imgs.size(0), 1, device=device, dtype=torch.float32)
-            fake = torch.zeros(real_imgs.size(0), 1, device=device, dtype=torch.float32)
+                # Target
+                real = torch.ones(real_imgs.size(0), 1, device=device, dtype=torch.float32)
+                fake = torch.zeros(real_imgs.size(0), 1, device=device, dtype=torch.float32)
 
-            # Noise(z) sampling from normal distribution
-            z = torch.tensor(
-                np.random.normal(0, 1, size=(real_imgs.size(0), opt.latent_dim)),
-                device=device,
-                dtype=torch.float32,
+                # Noise(z) sampling from normal distribution
+                z = torch.tensor(
+                    np.random.normal(0, 1, size=(real_imgs.size(0), opt.latent_dim)),
+                    device=device,
+                    dtype=torch.float32,
+                )
+
+                # Generate imgs
+                fake_imgs = generator(z)
+
+                # ** Iteration for discriminator **
+                optimizer_D.zero_grad()
+                real_loss = loss(discriminator(real_imgs), real)  # -log(D(x)
+                fake_loss = loss(discriminator(fake_imgs.detach()), fake)  # -log(1-D(G(z)))
+                loss_D = real_loss + fake_loss  # -(log(D(x)) + log(1-D(G(z))))
+                loss_D.backward()
+                optimizer_D.step()
+
+                #  ** Iteration for generator **
+                optimizer_G.zero_grad()
+                loss_G = loss(discriminator(fake_imgs), real)  # -log(D(G(z)))
+                loss_G.backward()
+                optimizer_G.step()
+
+                running_loss_G += loss_G.item()
+                running_loss_D += loss_D.item()
+
+                # tqdm
+                pbar.set_postfix(
+                    {
+                        "Generator loss": "%.3f" % loss_G.item(),
+                        "Discriminator loss": "%.3f" % loss_D.item(),
+                    }
+                )
+
+                # TODO Tensorboard, logging
+                if i % 10 == 9:  # every 10 minibatch
+                    # log = f"Epoch: {epoch}/{opt.n_epochs}, loss_G: {running_loss_G/10}, loss_D: {running_loss_D/10}"
+                    # print(log)
+
+                    writer.add_scalar(
+                        "Generator loss", running_loss_G / 10, epoch * len(train_loader) + i
+                    )
+                    writer.add_scalar(
+                        "Discriminator loss", running_loss_D / 10, epoch * len(train_loader) + i
+                    )
+
+                    running_loss_G = 0.0
+                    running_loss_D = 0.0
+
+            scheduler_D.step()
+            scheduler_G.step()
+            pbar.update(1)
+
+            # Save fake images
+            fake_img_dir = os.path.join("./images", opt.dataset)
+            os.makedirs(fake_img_dir, exist_ok=True)
+            save_image(
+                fake_imgs.data[:25], f"{fake_img_dir}/epoch_{epoch}.png", nrow=5, normalize=True
             )
-
-            # Generate imgs
-            fake_imgs = generator(z)
-
-            # ** Iteration for discriminator **
-            optimizer_D.zero_grad()
-            real_loss = loss(discriminator(real_imgs), real)  # -log(D(x)
-            fake_loss = loss(discriminator(fake_imgs.detach()), fake)  # -log(1-D(G(z)))
-            loss_D = real_loss + fake_loss  # -(log(D(x)) + log(1-D(G(z))))
-            loss_D.backward()
-            optimizer_D.step()
-
-            #  ** Iteration for generator **
-            optimizer_G.zero_grad()
-            loss_G = loss(discriminator(fake_imgs), real)  # -log(D(G(z)))
-            loss_G.backward()
-            optimizer_G.step()
-
-            running_loss_G += loss_G.item()
-            running_loss_D += loss_D.item()
-
-            # TODO Tensorboard, logging
-            if i % 10 == 9:  # every 10 minibatch
-                log = f"Epoch: {epoch}/{opt.n_epochs}, loss_G: {running_loss_G/10}, loss_D: {running_loss_D/10}"
-                print(log)
-
-                writer.add_scalar(
-                    "Generator loss", running_loss_G / 10, epoch * len(train_loader) + i
-                )
-                writer.add_scalar(
-                    "Discriminator loss", running_loss_D / 10, epoch * len(train_loader) + i
-                )
-
-                running_loss_G = 0.0
-                running_loss_D = 0.0
-
-        scheduler_D.step()
-        scheduler_G.step()
-
-        # Save fake images
-        fake_img_dir = os.path.join("./images", opt.dataset)
-        os.makedirs(fake_img_dir, exist_ok=True)
-        save_image(fake_imgs.data[:25], f"{fake_img_dir}/epoch_{epoch}.png", nrow=5, normalize=True)
 
 
 if __name__ == "__main__":
-    # Set device
+    # Set deviceshape and texture paramet
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(device)
 
